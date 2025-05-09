90006cfe123abb098cd0428874dbce071732d835
AntonGlinisty <glinistyi.ad@phystech.edu>
2025-05-09 20:07:12 +0300

    Refactor a bit

---
+++ first_task/clean.sh
@@ -0,0 +1,5 @@
+kubectl delete cj my-cronjob
+kubectl delete ds my-daemonset
+kubectl delete svc my-service
+kubectl delete deployment my-deployment
+kubectl delete cm my-configmap
---
+++ first_task/deploy.sh
@@ -0,0 +1,5 @@
+kubectl apply -f ./manifests/configmap.yaml
+kubectl apply -f ./manifests/deployment.yaml
+kubectl apply -f ./manifests/service.yaml
+kubectl apply -f ./manifests/daemonset.yaml
+kubectl apply -f ./manifests/cronjob.yaml
